cf93392a84a2f47c54461659d6472ccf4ed84e72
nduytung <nduytung.1611@gmail.com>
2021-02-22 10:43:54 +0700

    more layout

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <div className="h-screen w-2/3 text-black border border-gray-600 bg-white">
+      this is the sidebar testing message
+    </div>
+  );
+};
+
+export default SideBar;
--- src/container/layout/Header.jsx
+++ src/container/layout/Header.jsx
@@ -1,14 +1,21 @@
 import { GrMenu } from "react-icons/gr";
 import Image from "next/image";
+import SideBar from "../../components/SideBar";
 const Header = () => {
   // bg-gradient-to-r from-tomato via-coral to-yellow-400
   return (
-    <div className=" bg-white z-100 w-full flex items-center justify-between px-2 py-4">
-      <span>
-        <Image src="/logo.png" alt="" height="64" width="130" />
-      </span>
-      <div className="text-2xl">
-        <GrMenu />
+    <div className="relative">
+      <div className=" bg-white z-100 w-full flex items-center justify-between px-2 py-4">
+        <span>
+          <Image src="/logo.png" alt="" height="64" width="130" />
+        </span>
+        <div className="text-2xl ">
+          <GrMenu />
+        </div>
+      </div>
+      <div className="fixed top-0 right-0 w-full flex">
+        <div class="bg-gray-500 w-1/3 bg-opacity-50"></div>
+        <SideBar />
       </div>
     </div>
   );
